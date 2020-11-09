d10793a1d3a9f3dc78712f1279b74b360d533b1d
guoshusong <2460526449@qq.com>
2020-11-09 14:58:54 +0800

    feat(project): 完善接口

--- src/main/java/com/qingyuan/pigeon/mapper/UserMessageMapper.java
+++ src/main/java/com/qingyuan/pigeon/mapper/UserMessageMapper.java
@@ -3,17 +3,10 @@ package com.qingyuan.pigeon.mapper;
 import com.qingyuan.pigeon.pojo.User;
 
 /**
- * 对应数据库: user_message
+ * 对应数据库: user
  * 对应实体类:User
  * @author GuoShuSong
  */
 public interface UserMessageMapper {
-
-    /**
-     * 根据用户id获取用户信息
-     * @param userId
-     * @return
-     */
-    User getUserByUserId(Integer userId);
 
 }

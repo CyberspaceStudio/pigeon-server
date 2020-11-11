57489c8e951aa787dc1a4df67ffc4229a24272bb
QiuYiliang <1162922438@qq.com>
2020-11-11 09:39:09 +0800

    feat(team): 新增团队

--- src/main/java/com/qingyuan/pigeon/mapper/TeamMapper.java
+++ src/main/java/com/qingyuan/pigeon/mapper/TeamMapper.java
@@ -1,9 +1,17 @@
 package com.qingyuan.pigeon.mapper;
 
 
+import com.qingyuan.pigeon.pojo.Team;
+import org.apache.ibatis.annotations.Mapper;
+
 /**
  * 对应数据库:team team_member
  * @author 24605
  */
+@Mapper
 public interface TeamMapper {
+
+    int createTeam(Team team);
+
+    int updateTeamAvatar(Integer teamId, String avatarUrl);
 }
--- src/main/java/com/qingyuan/pigeon/service/impl/TeamServiceImpl.java
+++ src/main/java/com/qingyuan/pigeon/service/impl/TeamServiceImpl.java
@@ -1,13 +1,24 @@
 package com.qingyuan.pigeon.service.impl;
 
+import com.qingyuan.pigeon.enums.ResponseResultEnum;
+import com.qingyuan.pigeon.mapper.TeamMapper;
+import com.qingyuan.pigeon.pojo.Team;
 import com.qingyuan.pigeon.service.TeamService;
+import com.qingyuan.pigeon.utils.UniversalResponseBody;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.annotation.Resource;
+import java.io.File;
+import java.io.IOException;
 
 /**
  * 团队相关接口
  * @author 24605
  */
 @Service
+@Slf4j
 public class TeamServiceImpl  implements TeamService {
 
     /**
@@ -18,4 +29,35 @@ public class TeamServiceImpl  implements TeamService {
      * 团队头像的访问地址,直接在该URL后面加上teamId.png就可以
      */
     private static  final String USER_IMAGE_URL = "https://minimalist.net.cn/image-pigeon/team-avatar/";
+
+    @Resource
+    private TeamMapper teamMapper;
+
+    @Override
+    public UniversalResponseBody<Team> createTeam(Team team, MultipartFile multipartFile) {
+        int affectedRow = teamMapper.createTeam(team);
+        if (affectedRow < 1) {
+            return new UniversalResponseBody<>(ResponseResultEnum.FAILED.getCode(), ResponseResultEnum.FAILED.getMsg());
+        }
+
+        String teamFileName = team.getTeamId() + ".png";
+        String filePath = TEAM_AVATAR_DIR_PATH + teamFileName;
+        log.info("团队上传头像，路径为", filePath);
+
+        try {
+            multipartFile.transferTo(new File(filePath));
+        } catch (IOException e) {
+            e.printStackTrace();
+            return new UniversalResponseBody<>(ResponseResultEnum.FAILED.getCode(), ResponseResultEnum.FAILED.getMsg());
+        }
+
+        String imageUrl = USER_IMAGE_URL + filePath;
+        team.setTeamAvatarUrl(imageUrl);
+        int i = teamMapper.updateTeamAvatar(team.getTeamId(), imageUrl);
+        if (i > 0) {
+            return new UniversalResponseBody<>(ResponseResultEnum.SUCCESS.getCode(), ResponseResultEnum.SUCCESS.getMsg(), team);
+        }
+
+        return new UniversalResponseBody<>(ResponseResultEnum.FAILED.getCode(), ResponseResultEnum.FAILED.getMsg());
+    }
 }

363c7a4d290e1de137f6d378fc0cb234b1f8b275
guoshusong <2460526449@qq.com>
2020-11-17 20:30:55 +0800

    fix(task): 任务类改为任务开始时间、任务结束时间

--- src/main/java/com/qingyuan/pigeon/enums/TaskStatusEnum.java
+++ src/main/java/com/qingyuan/pigeon/enums/TaskStatusEnum.java
@@ -8,19 +8,16 @@ public enum TaskStatusEnum {
 
 
     /**
-     * 报名时间: 任务开始前6个小时之外
+     * 报名时间: 任务开始前2个小时之外
      * 签到时间: 任务开始前20分钟,任务开始后10分钟
      * 签退时间: 任务结束后30分钟之内
      * 过期时间: 任务结束后30分钟
      */
-
-    TASK_CREATED(0,"已创建"),
-    TASK_APPLY_TIME_PASS(1,"已过报名时间"),
-    TASK_NO_CHECK_IN_TIME(2,"未到签到时间"),
-    TASK_CHECK_IN_TIME_PASSED(3,"已过签到时间"),
-    TASK_NO_CHECK_OUT_TIME(4,"未到签退时间"),
-    TASK_CHECK_OUT_TIME_PASSED(5,"已过签退时间"),
-    TASK_EXPIRED(6,"已过期");
+    TASK_CREATED(0,"可以报名"),
+    TASK_NO_CHECK_IN_TIME(1,"可以签到"),
+    TASK_(2,"任务正在进行"),
+    TASK_NO_CHECK_OUT_TIME(3,"可以签退"),
+    TASK_EXPIRED(4,"已结束");
 
     /**
      * 活动状态对应的id

6df53f46833de9d199650f88dd59523b542b929b
guoshusong <2460526449@qq.com>
2020-11-17 19:15:03 +0800

    docs(task): 增加任务状态

--- src/main/java/com/qingyuan/pigeon/enums/TaskStatusEnum.java
+++ src/main/java/com/qingyuan/pigeon/enums/TaskStatusEnum.java
@@ -6,7 +6,9 @@ package com.qingyuan.pigeon.enums;
  */
 public enum TaskStatusEnum {
     TASK_CREATED(0,"已创建"),
-    TASK_EXPIRED(2,"已过期");
+    TASK_APPLY_TIME(1,"已过报名时间"),
+    TASK_CHECK_TIME_PASSES(2,"已过签到时间"),
+    TASK_EXPIRED(3,"已过期");
 
     /**
      * 活动状态对应的id
